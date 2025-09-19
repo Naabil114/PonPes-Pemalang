2e61e3ec9edb06d4b38e6fe16bdcda36ed5033a7
nabil <nabiltrobel.23@gmail.com>
2025-09-19 13:43:55 +0700

    FEBF(data-santri-blm-fix)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SantriController;
+use App\Http\Controllers\DashboardController;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('dashboard',[DashboardController::class,'index']);
+Route::get('data-santri',[SantriController::class,'index']);
